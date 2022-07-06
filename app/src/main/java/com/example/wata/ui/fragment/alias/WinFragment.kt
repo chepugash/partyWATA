04ceeaa981e2544b049e90fda81aef31c9583ad9
chepugash <chepugash@yahoo.com>
2022-07-06 20:19:13 +0300

    navigation fixed, score added at round

--- app/src/main/java/com/example/wata/ui/fragment/alias/WinFragment.kt
+++ app/src/main/java/com/example/wata/ui/fragment/alias/WinFragment.kt
@@ -1,7 +1,30 @@
 package com.example.wata.ui.fragment.alias
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.wata.R
+import com.example.wata.databinding.FragmentAliasRoundBinding
+import com.example.wata.databinding.FragmentAliasWinBinding
 
 class WinFragment : Fragment(R.layout.fragment_alias_win) {
+    private var _binding: FragmentAliasWinBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentAliasWinBinding.bind(view)
+
+        with(binding) {
+            btnMenu.setOnClickListener {
+                findNavController().navigate(R.id.action_winFragment_to_menuFragment)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
 }
