d14b1d2ae26009dfb3ebd953b5f8c373ff0a43fc
Ivan Fedotov <90091175+chepugash@users.noreply.github.com>
2022-07-06 21:43:40 +0300

    Revert "added alias first change"

--- app/src/main/java/com/example/wata/ui/fragment/alias/TeamsFragment.kt
+++ app/src/main/java/com/example/wata/ui/fragment/alias/TeamsFragment.kt
@@ -1,11 +1,7 @@
 package com.example.wata.ui.fragment.alias
 
-import android.os.Bundle
-import android.widget.Button
-import androidx.annotation.ContentView
 import androidx.fragment.app.Fragment
 import com.example.wata.R
 
 class TeamsFragment : Fragment(R.layout.fragment_alias_teams) {
-
 }
