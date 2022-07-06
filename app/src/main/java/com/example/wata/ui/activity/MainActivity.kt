0650e3bb1641c0ab7694e6e900cbc97e9b867f36
chepugash <chepugash@yahoo.com>
2022-07-06 15:15:44 +0300

    round fragment updated

--- app/src/main/java/com/example/wata/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/wata/ui/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.wata.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.wata.R
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
+            controller = (supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment).navController
+
         }
     }
 }
--- app/src/main/java/com/example/wata/ui/fragment/alias/RoundFragment.kt
+++ app/src/main/java/com/example/wata/ui/fragment/alias/RoundFragment.kt
@@ -1,7 +1,24 @@
 package com.example.wata.ui.fragment.alias
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.example.wata.R
+import com.example.wata.databinding.FragmentAliasRoundBinding
 
 class RoundFragment : Fragment(R.layout.fragment_alias_round) {
+    private var _binding: FragmentAliasRoundBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentAliasRoundBinding.bind(view)
+
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
 }
