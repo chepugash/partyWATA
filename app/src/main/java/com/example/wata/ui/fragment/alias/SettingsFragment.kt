d2b0d7edfb996d87a25799cce4daaa8968fd3b5d
Iluyza <90064161+Iluyza@users.noreply.github.com>
2022-07-07 23:46:42 +0300

    added setting fragment

--- app/src/main/java/com/example/wata/ui/fragment/alias/SettingsFragment.kt
+++ app/src/main/java/com/example/wata/ui/fragment/alias/SettingsFragment.kt
@@ -1,7 +1,64 @@
 package com.example.wata.ui.fragment.alias
 
+import android.os.Bundle;
+import android.view.View
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.wata.R
+import com.example.wata.databinding.FragmentAliasSettingsBinding
 
 class SettingsFragment : Fragment(R.layout.fragment_alias_settings) {
+
+    private var _binding: FragmentAliasSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentAliasSettingsBinding.bind(view)
+        with(binding) {
+            seekBarTime.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    tvTime.text = progress.toString()
+                }
+
+                override fun onStartTrackingTouch(seekBarTime: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+            })
+            seekBarWord.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    tvCounter.text = progress.toString()
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+
+            })
+            ibtnLeft.setOnClickListener {
+                findNavController().navigate(
+                    R.id.action_settingsFragment_to_teamsFragment
+                )
+            }
+            btnContinue.setOnClickListener {
+                findNavController().navigate(
+                    R.id.action_settingsFragment_to_preroundFragment
+                )
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/wata/ui/fragment/alias/teamlist/Team.kt
+++ app/src/main/java/com/example/wata/ui/fragment/alias/teamlist/Team.kt
@@ -1,6 +1,7 @@
 package com.example.wata.ui.fragment.alias.teamlist
 
 data class Team(
+    val id: Int,
     var name: String,
-    val points: Int
+    var points: Int
 )
