634b8acae871614f27dbf478a45a6191b3def822
chepugash <chepugash@yahoo.com>
2022-07-08 03:56:32 +0300

    countdown fragment deleted

--- app/src/main/java/com/example/wata/ui/fragment/alias/RoundFragment.kt
+++ app/src/main/java/com/example/wata/ui/fragment/alias/RoundFragment.kt
@@ -1,6 +1,7 @@
 package com.example.wata.ui.fragment.alias
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.SystemClock
 import android.view.View
 import android.widget.Chronometer
@@ -23,11 +24,10 @@ class RoundFragment : Fragment(R.layout.fragment_alias_round) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentAliasRoundBinding.bind(view)
 
-        val args by navArgs<RoundFragmentArgs>()
-        val team_id = args.teamId
-        val round_time = args.roundTime
-        val points_for_win = args.pointsForWin
-        val round = args.round
+        val team_id = 0
+        val round_time = 10
+        val points_for_win = 50
+        val round = 1
 
         var score = 0
 
@@ -35,40 +35,76 @@ class RoundFragment : Fragment(R.layout.fragment_alias_round) {
         var word = "Слово $counter"
 
         with(binding) {
+            object : CountDownTimer(4000, 1000) {
+                override fun onTick(milliseconds: Long) {
+                    val s: Long = milliseconds % 60000 / 1000
+                    tvCountdown.text = String.format(s.toString())
+                }
+                override fun onFinish() {
+                    ivBottom.visibility = View.VISIBLE
+                    chrTimer.visibility = View.VISIBLE
+                    tvWord.visibility = View.VISIBLE
+                    btnTrue.visibility = View.VISIBLE
+                    btnFalse.visibility = View.VISIBLE
+                    tvCountdown.visibility = View.INVISIBLE
+                    initChronometer(round_time)
+                }
+            }.start()
+
             tvScore.text = score.toString()
             tvWord.text = word
             tvTeam.text = TeamRepository.teams[team_id].name
+
             btnTrue.setOnClickListener {
                 score++
-                counter = Random.nextInt(0, 100)
-                word = "Слово $counter"
-                tvScore.text = score.toString()
-                tvWord.text = word
+                if (chrTimer.visibility == View.INVISIBLE) {
+                    val action = RoundFragmentDirections.actionRoundFragmentToPreroundFragment(team_id, round_time, points_for_win, score, round)
+                    binding.root.findNavController().navigate(action)
+                } else {
+                    counter = Random.nextInt(0, 100)
+                    word = "Слово $counter"
+                    tvScore.text = score.toString()
+                    tvWord.text = word
+                }
             }
+
             btnFalse.setOnClickListener {
-                counter = Random.nextInt(0, 100)
                 score--
-                tvScore.text = score.toString()
-                word = "Слово $counter"
-                tvWord.text = word
+                if (chrTimer.visibility == View.INVISIBLE) {
+                    val action = RoundFragmentDirections.actionRoundFragmentToPreroundFragment(team_id, round_time, points_for_win, score, round)
+                    binding.root.findNavController().navigate(action)
+                } else {
+                    counter = Random.nextInt(0, 100)
+                    tvScore.text = score.toString()
+                    word = "Слово $counter"
+                    tvWord.text = word
+                }
             }
         }
-
-        initChronometer(team_id, round_time, points_for_win, score, round)
     }
 
-    private fun initChronometer(team_id: Int, round_time: Int, points_for_win: Int, score: Int, round: Int) {
+    private fun initChronometer(round_time: Int) {
         chronometer = binding.chrTimer
         chronometer.isCountDown = true
         chronometer.base = SystemClock.elapsedRealtime() + round_time * 1000
         chronometer.start()
         chronometer.setOnChronometerTickListener {
-            if (chronometer.text.toString() == "00:00") {
+            if (chronometer.text.toString() == "00:00" || chronometer.text.toString()[3] == ':') {
                 chronometer.stop()
-                val action = RoundFragmentDirections.actionRoundFragmentToPreroundFragment(team_id, round_time, points_for_win, score, round)
-                binding.root.findNavController().navigate(action)
+                chronometer.visibility = View.INVISIBLE
+                binding.tvLastWord.visibility = View.VISIBLE
             }
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.chrTimer.stop()
+    }
+
+    override fun onResume() {
+        super.onStart()
+        binding.chrTimer.start()
     }
 
     override fun onDestroy() {
